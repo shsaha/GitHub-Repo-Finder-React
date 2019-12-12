4eff8890ff60fccc5f7d79b9490e877201cbea47
Shaswata Saha <shsaha@ttu.ee>
2019-12-12 19:38:29 +0530

    All files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import UserForm from './components/UserForm';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    repos: null
+  }
+
+  getUser = (e) => {
+    e.preventDefault();
+    const user = e.target.elements.username.value;
+    //console.log(user);
+    if (user) {
+      axios.get(`https://api.github.com/users/${user}`)
+        .then((res) => {
+          //console.log(res);
+          const repos = res.data.public_repos;
+          //console.log(repos);
+          this.setState({ repos: repos })
+        })
+    } else return;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">GitHub Repo Finder</h1>
+        </header>
+        <UserForm getUser={this.getUser} />
+        {this.state.repos ? <p>Number of repos: {this.state.repos}</p>: <p>Please enter a username</p>}
+
+      </div>
+    );
+  }
 }
 
 export default App;
